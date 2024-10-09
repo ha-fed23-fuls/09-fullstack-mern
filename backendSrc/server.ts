e01cac02c0dd8ad23c24692338502d73121bccdf
David Andersson <lejonmanen@gmail.com>
2024-10-09 10:09:15 +0200

    Server serves static files

--- backendSrc/server.ts
+++ backendSrc/server.ts
@@ -1,2 +1,14 @@
-console.log('Hello world from backend')
+import express, { Express } from 'express'
 
+const app: Express = express()
+const port: number = 4242
+
+app.use('/', express.static('dist/'))
+
+
+
+
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}...`)
+})
